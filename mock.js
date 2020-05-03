b29052debacd6e32c8bdd7e01da438c5aadc44db
wangyongqi <wangyongqi@baidu.com>
2020-05-03 12:33:42 +0800

    add backend mock

---
+++ mock.js
@@ -0,0 +1,30 @@
+/** * @author: wangyongqi@baidu.com
+ * @date: 2020-05-03 01:13:04
+ * @last Modified time: 2020-05-03 01:13:04
+ */
+
+const express = require('express');
+
+const app = express();
+const port = 9000;
+
+app.use('/api', (req, res) => {
+
+
+    return res.json(
+        {
+            posts: [
+                '1111111',
+                '22222',
+                '3333333'
+            ]
+        }
+    )
+});
+
+
+app.listen(port, () => {
+    console.log('mock server has started!, listen port: ', port);
+
+});
+
