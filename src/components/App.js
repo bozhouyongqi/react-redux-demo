1bb82aedc136e3807d1890ccc5c4138a4ae23cf9
wangyongqi <wangyongqi@baidu.com>
2020-05-16 01:03:31 +0800

    init 新代码

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,14 @@
 
-import React from 'react';
-import store from '../store';
-import { Provider, connect } from 'react-redux';
-import Picker from './Picker';
-import Posts from './Posts';
-import {selectSubreddit} from '../actions/selectSubreddit';
-import fetchPostsIfNeeded from '../actions/postBySubreddit';
+import React, { Component } from 'react'
 
-function App(props) {
 
-    console.log('App', props);
-    const { selectedSubreddit, posts, isFetching, lastUpdated, dispatch} = props;
-    const isEmpty = posts.length === 0;
-
-    // TO DO
-    const handleChange = value => {
-        // dispatch select and dispatch postbysubreffit
-        dispatch(selectSubreddit(value));
-        dispatch(fetchPostsIfNeeded(value));
-    };
-
-    const handleRefreshClick = () => {
-        dispatch(selectSubreddit(selectedSubreddit));
-        dispatch(fetchPostsIfNeeded(selectedSubreddit));
+// 必须包含React，是因为babel要利用React中提供的CreateElement方法创建虚拟dom
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                welcome to App.
+            </div>
+        );
     }
-    return (
-        <div>
-            <Picker value={selectedSubreddit}
-                onChange={handleChange}
-                options={['reactjs', 'frontend']}>
-            </Picker>
-            <p>
-                {
-                    lastUpdated && 
-                    <span>
-                        lastUpdated at {new Date(lastUpdated).toLocaleTimeString()}.
-                        {' '}
-                    </span>
-                }
-                {
-                    !isFetching &&
-                    <a href="#"
-                        onClick={handleRefreshClick}>
-                        Refresh
-                    </a>
-                }
-            </p>
-            {
-                isEmpty ? (isFetching ? <h2> Loading...</h2> : <h2>Empty.</h2>)
-                    : <div style={{opacity: isFetching ? 0.5 : 1}}>
-                        <Posts posts={posts}></Posts>
-                    </div>
-            }
-        </div>
-    );
 }
-
-const mapStateToProps = state => {
-    const {selectedSubreddit, postsBySubreddit = {}} = state;
-    const {
-        isFetching,
-        lastUpdated,
-        items: posts
-    } = postsBySubreddit[selectedSubreddit] || {
-        isFetching: false,
-        items: []
-    };
-
-    return {
-        selectedSubreddit,
-        posts,
-        isFetching,
-        lastUpdated
-    };
-};
-
-
-const WrapperApp = connect(mapStateToProps)(App);
-
-export default function () {
-
-    return (
-        <Provider store={store}>
-            <WrapperApp />
-        </Provider>
-    );
-};
-
